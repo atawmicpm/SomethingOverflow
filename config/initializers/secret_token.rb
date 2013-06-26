729ecef95baffc868254d96883c9641e89a3de55
nathaniel <nathanielwroblewski@gmail.com>
2013-06-26 09:47:36 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SomethingOverflow::Application.config.secret_token = '63a754e24be00698a63727fd4d924d2cb7be2da2959fe1ba5fd86151ea3f1e9dc12e3028a5db772e348c04c94841f6bfc94a5530ac2ad3ebda1a6c1d480fd818'
