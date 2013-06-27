b9d46ed1014cf8e47414eb415a75a899e4b1b2b4
dbcapprentice <dbcapprentice@dbc.com>
2013-06-27 00:21:20 -0700

    styled header (for all pages) and index page

--- Gemfile
+++ Gemfile
@@ -10,10 +10,10 @@ gem 'pg'
 group :development, :test do
   gem 'rspec-rails'
   gem 'factory_girl_rails'
+  gem 'faker'
 end
 
 group :test do
-  gem 'faker'
   gem 'capybara'
   gem 'guard-rspec'
   gem 'launchy'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(name: "Foo Bar", email: "foo@bar.com", password: "password")
+User.create(name: "Bar Foo", email: "bar@foo.com", password: "password")
+User.create(name: "Foo Baz", email: "foo@baz.com", password: "password")
+
+Question.create(title: "Best smartphone on the market today",
+                content: Faker::Lorem.paragraph)
+Question.create(title: "If you could recommend one personal product/accessory, what would it be?",
+                content: Faker::Lorem.paragraph)
+Question.create(title: "Looking to buy a banjo.  Which one should I get?",
+                content: Faker::Lorem.paragraph)
+Question.create(title: "Anyone have suggestions on what SUV I should buy?",
+                content: Faker::Lorem.paragraph)
+Question.create(title: "Product for wet hair look?",
+                content: Faker::Lorem.paragraph)
+Question.create(title: "What are the new and upcoming trends in men's high fashion?",
+                content: Faker::Lorem.paragraph)
+Question.create(title: "Advice for buying a new peacoat",
+                content: Faker::Lorem.paragraph)
+Question.create(title: "Male Skin Care Products",
+                content: Faker::Lorem.paragraph)
+Question.create(title: "Looking to buy new speakers for my home. Any advice?",
+                content: Faker::Lorem.paragraph)
+Question.create(title: "What snowboard should I get?",
+                content: Faker::Lorem.paragraph)
+
+Question.all.each do |question|
+  user = User.find(rand(1..3))
+  user.questions << question
+end
+
+Answer.create(url: "http://www.abc-of-snowboarding.com/images/content-images/article-unitysnowboards-1.gif", content: Faker::Lorem.paragraph)
+Answer.create(url: "http://images.ccs.com/pi/65-05324/large/lib-tech-burtner-box-scratcher-btx-snowboard-mens", content: Faker::Lorem.paragraph)
+Answer.create(url: "http://di1.shopping.com/images/pi/d0/de/db/31046502-200x200-0-0.jpg?p=p7.95a38750b00b7d0cd950&a=2&c=1&l=8055916&t=100108133712&r=1", content: Faker::Lorem.paragraph)
+Answer.create(url: "http://images.ccs.com/pi/65-12117/large/burton-process-snowboard-custom-binding-pkg-mens", content: Faker::Lorem.paragraph)
+Answer.create(url: "http://www.snowshedny.com/ProductImages/112524/c_6117249_burton-lux-snowboard-women-s-2013-139-front.jpg", content: Faker::Lorem.paragraph)
+
+Answer.all.each do |answer|
+  user = User.find(rand(1..3))
+  question = Question.last
+  user.answers << answer
+  question.answers << answer
+end
