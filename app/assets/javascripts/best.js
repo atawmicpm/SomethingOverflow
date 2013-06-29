f24c1c1666be1a619cc1805cad304f2fe7ada960
Phillip Mispagel <atawmicpm@gmail.com>
2013-06-28 21:19:28 -0700

    updates to make answers sortable by best and give owner the ability to choose best answer

--- app/assets/javascripts/best.js
+++ app/assets/javascripts/best.js
@@ -1,25 +1,30 @@
 var Best = {
 
 	init: function() {
+		this.setBest();
 		$('.best_answer').on('ajax:success', this.chooseBest)
 	},
 
 	chooseBest: function() {
-
+		$('.best_answer').show();
+		$('h2').hide();
+		var bestAnswer = $('h2').clone();
+		$('.answer').css('border', '1px solid white');
+		$('.answer').css('background-color', '#eee');
+		$(this).hide();
+		$(this).closest('.answer').css('background-color','#DCF0F7');
+		$(this).closest('.answer').css('border','1px solid lightblue');
+		$(this).closest('.answer').prepend($bestAnswer);
 	},
 
-	addBest: function() {
+	setBest: function() {
 
-	},
-
-	removeBest: function() {
-		
+		$('#best-answer').css('background-color','#DCF0F7');
+		$('#best-answer').css('border','1px solid lightblue');
+		$('#best-answer').children().find('.best_answer').hide();
 	}
 
 }
-
-
-
 
 $(document).ready(function() {
   Best.init();
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,13 +3,19 @@ class Question < ActiveRecord::Base
   has_many :answers
 
   attr_accessible :title, :content, :user, :answer, :best_answer
-
   validates :title, :content, presence: true
-
   default_scope order('created_at DESC')
 
   def owner?(current_user)
   	current_user.id == self.user_id
+  end
+
+  def sorted_answers
+  	answers = self.answers
+  	best = answers.select { |answer| answer.id == self.best_answer }
+  	not_best = answers.reject! { |answer| answer.id == self.best_answer }
+  	not_best.sort!{ |a,b| b.vote_count <=> a.vote_count }.reverse
+  	not_best.unshift(best[0])
   end
 
 end
