5452ed0d16112456b0f6119745e87f763f567975
dbcapprentice <dbcapprentice@dbc.com>
2013-06-29 18:03:44 -0700

    fixed bug in answers/comments models where ids were exposed as attr_accessibles

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,11 +1,13 @@
 class AnswersController < ApplicationController
   def create
-    @answer = Answer.new(params[:answer])
-    @comment = Comment.new
-    if @answer.save
-      render :json => render_to_string(:partial => 'answer', :locals => { :answer => @answer, :@comment => @comment }).to_json
+    params[:answer][:question] = Question.find(params[:answer].delete(:question_id))
+    params[:answer][:user] = current_user
+    answer = Answer.create(params[:answer])
+    if answer.valid?
+      @comment = Comment.new
+      render :json => render_to_string(:partial => 'answer', :locals => { :answer => answer, :@comment => @comment }).to_json
     else
-      render :json => render_to_string(:partial => 'layouts/errors', :locals => { :bad_object => @answer }), :status => :unprocessable_entity
+      render :json => render_to_string(:partial => 'layouts/errors', :locals => { :bad_object => answer }), :status => :unprocessable_entity
     end
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,10 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :answer
   has_many   :votes, as: :voteable
 
-  # SHADI REVIEW: don't user user_id or *_id, try to use the name of the association.
-  # so this line would be attr_accessible :content, :user, :answer
-  # now when you mass assign: Comment.new :user => current_user, :answer => @answer.  
-  attr_accessible :content, :user_id, :answer_id
+  attr_accessible :content, :user, :answer
   validates :content, presence: true
 
   default_scope order('created_at DESC')
